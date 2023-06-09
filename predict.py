8574790780e26975e07054e221ee6abbc4aa044c
tongxinoptica <90898694+tongxinoptica@users.noreply.github.com>
2023-06-09 10:59:02 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ import torch
 from torchvision import transforms
 from swin_IR import swinIR
 # from unet_model import Unet
-# from model import RDR_model
+# from urdn_model import RDR_model
 from unit import to_psnr, to_ssim_skimage, to_ssim, to_pearson
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -10,7 +10,7 @@ import torch.nn as nn
 import openpyxl
 
 # wb = openpyxl.Workbook()
-# ws = wb.create_sheet('celeb')  # 新建表格名称
+# ws = wb.create_sheet('celeb')  # bulid a new excel
 # ws.cell(row=2, column=1).value = "mse"
 # ws.cell(row=3, column=1).value = "psnr"
 # ws.cell(row=4, column=1).value = "npcc"
@@ -19,30 +19,28 @@ import openpyxl
 
 # Change parameter
 parameter = '1.1f'
-name = ['4熊', '339飞机', '666人', '2893风景', '5642人', '5864碗', '6716叶子', '8544番茄']
-num = '5642'
 
 Tensor = transforms.Compose([transforms.Resize((128, 128)), transforms.ToTensor()])
 model = swinIR()
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # 定义训练的设备
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # Deifne the device
 model.to(device)
-print("模型加载完成")
+print("Model loaded")
 
-Image_path = 'C:/Users/Administrator/Desktop/paper3/tur_image/ocean/0.1/jpg/{}_in.jpg'.format(num)
-label_path = 'C:/Users/Administrator/Desktop/paper3/tur_image/jpg/{}.jpg'.format(num)
+Image_path = '' # Input path
+label_path = '' # Groundtruth path
 img = Image.open(Image_path)
 if img.mode=='RGB':
     img = img.convert('L')
 img = Tensor(img)
-img = img / (torch.max(img))  # 归一化0-1
+img = img / (torch.max(img))  # ImageNor
 img = torch.reshape(img, (1, 1, 128, 128))
 
 label = Image.open(label_path)
 if label.mode=='RGB':
     label = label.convert('L')
 label = Tensor(label)
-#label = label / (torch.max(label))  # 归一化0-1
-label = torch.reshape(label, (1, 1, 128, 128))  # 增加batch维度
+label = label / (torch.max(label))  
+label = torch.reshape(label, (1, 1, 128, 128))
 
 model.load_state_dict(torch.load('./parameter/lr=0.0005_{}_nor'.format(parameter)))
 #model.load_state_dict(torch.load('./parameter/swin_noconv_{}'.format(parameter)))
@@ -73,11 +71,11 @@ with torch.no_grad():
     # ws.cell(row=4, column=s).value = ssim[0]
     # ws.cell(row=5, column=s).value = npcc.item()
     to_pil = transforms.ToPILImage()
-    output_pil = to_pil(output[0])  # 删去batch维度
+    output_pil = to_pil(output[0]) 
     gt = to_pil(label[0])
     plt.imshow(output_pil, cmap='gray')
     plt.axis('off')
-    plt.savefig('C:/Users/Administrator/Desktop/paper3/tur_image/compare/our/{}_out_o0.1.jpg'.format(num),bbox_inches='tight', pad_inches=0)
+    plt.savefig('') # save path
     plt.show()
 
 
